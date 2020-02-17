6ec998cf63dafecda2e9ed4bcf2e580ba1ab36b0
Will Radi <williamradi@gmail.com>
2020-02-17 16:14:09 -0300

    Regristro de progresso

--- EnglishGame/src/app/painel/painel.component.ts
+++ EnglishGame/src/app/painel/painel.component.ts
@@ -9,21 +9,33 @@ import { FRASES } from './frases-mock';
 })
 export class PainelComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
   public instrucao: string = 'Traduza a frase:'
   public frases: Frase[] = FRASES
   public resposta: string
+  public rodada: number = 0
+  public rodadaFrase: Frase
+  public progresso: number = 0
+
+  constructor() {
+    this.rodadaFrase = this.frases[this.rodada]
+  }
+
+  ngOnInit(): void {
+  }
 
   atualizaResposta(texto: Event): void {
     this.resposta = (<HTMLInputElement>texto.target).value
   }
 
   comparaResposta(): void {
-    console.log(`Resposta: ${this.resposta}`)
+    if (this.resposta == this.rodadaFrase.frasePtBr) {
+      this.rodada++
+      this.rodadaFrase = this.frases[this.rodada]
+      this.progresso = this.progresso + (100 / this.frases.length)
+      console.log(this.progresso)
+    } else {
+      alert('Eroooou')
+    }
   }
 
 }
