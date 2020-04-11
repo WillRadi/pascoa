52cbb4cccdcc977a11058b227cff07b619f4b0da
Will Radi <williamradi@gmail.com>
2020-04-10 21:25:57 -0300

    Deploy e metadinha das frases

--- src/app/painel/frases-mock.ts
+++ src/app/painel/frases-mock.ts
@@ -1,8 +1,17 @@
 import { Frase } from '../shared/frase.model'
 
 export const FRASES: Frase[] = [
-    { fraseEn: 'I like to learn' , frasePtBr: 'Eu gosto de aprender' },
-    { fraseEn: 'I watch tv' , frasePtBr: 'Eu assisto tv' },
-    { fraseEn: 'How are you?' , frasePtBr: 'Como vai você' },
-    { fraseEn: 'I eat bread' , frasePtBr: 'Eu como pão' }
+    { fraseEn: 'Achou q a pascoa seria sem graça!? Achou errado! O que seria da nossa páscoa sem a CAÇA AOS OVOS?? Vamos lá! Clica aí em "Próxima pista" e inicie nossa jornada!' , frasePtBr: '' },
+    { fraseEn: '[aparador] - A temática desse ano é... NENÉM!! Então, me diga: onde nosso neném passou a gostar de descançar/nanar de vez em quando?' , frasePtBr: '' },
+    { fraseEn: '[berço] - Começou bem! Fácil, né?' , frasePtBr: '' },
+    { fraseEn: '[mesinha]' , frasePtBr: '' },
+    { fraseEn: '[totoka/uni]' , frasePtBr: '' },
+    { fraseEn: '[fala comigo]' , frasePtBr: '' },
+    { fraseEn: '[geladeira]' , frasePtBr: '' },
+    { fraseEn: '[porta]' , frasePtBr: '' },
+    { fraseEn: '[livros]' , frasePtBr: '' },
+    { fraseEn: '[espelho]' , frasePtBr: '' },
+    { fraseEn: '[guitarrinha]' , frasePtBr: '' },
+    { fraseEn: '[sapatinho]' , frasePtBr: '' },
+    { fraseEn: '[allstar]' , frasePtBr: '' }
 ]
