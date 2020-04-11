ea6d335a5f49d18a3ef10e40dffc10952e2ffafa
Will Radi <williamradi@gmail.com>
2020-04-10 22:16:09 -0300

    nome no package.json e frases sem senha

--- src/app/painel/frases-mock.ts
+++ src/app/painel/frases-mock.ts
@@ -3,15 +3,14 @@ import { Frase } from '../shared/frase.model'
 export const FRASES: Frase[] = [
     { fraseEn: 'Achou q a pascoa seria sem graça!? Achou errado! O que seria da nossa páscoa sem a CAÇA AOS OVOS?? Vamos lá! Clica aí em "Próxima pista" e inicie nossa jornada!' , frasePtBr: '' },
     { fraseEn: '[aparador] - A temática desse ano é... NENÉM!! Então, me diga: onde nosso neném passou a gostar de descançar/nanar de vez em quando?' , frasePtBr: '' },
-    { fraseEn: '[berço] - Começou bem! Fácil, né?' , frasePtBr: '' },
-    { fraseEn: '[mesinha]' , frasePtBr: '' },
-    { fraseEn: '[totoka/uni]' , frasePtBr: '' },
-    { fraseEn: '[fala comigo]' , frasePtBr: '' },
-    { fraseEn: '[geladeira]' , frasePtBr: '' },
-    { fraseEn: '[porta]' , frasePtBr: '' },
-    { fraseEn: '[livros]' , frasePtBr: '' },
-    { fraseEn: '[espelho]' , frasePtBr: '' },
-    { fraseEn: '[guitarrinha]' , frasePtBr: '' },
-    { fraseEn: '[sapatinho]' , frasePtBr: '' },
-    { fraseEn: '[allstar]' , frasePtBr: '' }
+    { fraseEn: '[berço] - Começou bem! Fácil, né? Agora é ao contrário... Onde ela não gosta de dormir?!' , frasePtBr: '' },
+    { fraseEn: '[mesinha] - Parabéns, mamãe!! Agora, pra puxar o saco da mamãe um pouco, o que o neném gosta de fazer mais com a mamãe do que com o papai? Dica: ela já se arruma sozinha pra isso!' , frasePtBr: '' },
+    { fraseEn: '[totoka/uni] - Boa!! A próxima é fácil e a dica é curta: "mãmã"... Corre lá!' , frasePtBr: '' },
+    { fraseEn: '[fala comigo] - Te peguei, né!? Agora vai lá no seu amor e da um beijinho nele.. vai que ele solta essa senha de graça...' , frasePtBr: '' },
+    { fraseEn: '[porta] - Depois da senha mais fácil de todas, vamos voltar à dificuldade. Veja com a nossa artista se ela sabe dessa senha!' , frasePtBr: '' },
+    { fraseEn: '[livros] - Lindo desenho, não? Mas... Falando em arte, qual a arte que o neném anda fazendo que mais atrapalha o papai?!' , frasePtBr: '' },
+    { fraseEn: '[espelho] - Difícil trabalhar com o neném em cima da mesa, né... Mas vamos lá! Dica fácil pq tbm quero comer CHOCOLATE: "laaava a mão"' , frasePtBr: '' },
+    { fraseEn: '[guitarrinha] - Agora uma difícil e não muito relacionada ao tema NENÉM, mas a próxima senha está num lugar tudo a ver com chocolate e com nosso amor! S2' , frasePtBr: '' },
+    { fraseEn: '[sapatinho] - Saudades da nossa viagem, né... ai ai... Chega de saudade! Tá acabando o jogo e sei que o que vc quer é OVO DE PÁSCOA! O que é, o que é: Anda amarrado e para desamarrado? Dica: Voltamos ao tema, é algo do neném!' , frasePtBr: '' },
+    { fraseEn: '[allstar] - Última pista!! Quero te fazer CAMINHAR pela casa pra procurar! Dica: você ainda estava prenha do neném! *---*' , frasePtBr: '' }
 ]
